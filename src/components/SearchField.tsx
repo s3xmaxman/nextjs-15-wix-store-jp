e814cbb7f419641c216062a41663c75a4323a58a
s3xmaxman <badside3919@gmail.com>
2024-11-10 19:51:45 +0900

    feat: create SearchField.tsx

---
+++ src/components/SearchField.tsx
@@ -0,0 +1,39 @@
+"use client ";
+import { cn } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { Input } from "./ui/input";
+import { SearchIcon } from "lucide-react";
+
+interface SearchFieldProps {
+  className?: string;
+}
+
+export default function SearchField({ className }: SearchFieldProps) {
+  const router = useRouter();
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const formData = e.currentTarget;
+    const q = (formData.q as HTMLInputElement).value.trim();
+
+    if (!q) {
+      return;
+    }
+
+    router.push(`/shop?q=${encodeURIComponent(q)}`);
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      method="GET"
+      action="/shop"
+      className={cn("grow", className)}
+    >
+      <div className="relative">
+        <Input name="q" placeholder="Search" className="pe-10" />
+        <SearchIcon className="absolute right-3 top-1/2 size-5 -translate-y-1/2 transform text-muted-foreground" />
+      </div>
+    </form>
+  );
+}
