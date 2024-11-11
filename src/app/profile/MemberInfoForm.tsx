2f953e59b0c05052ba6702d437d74f8a06cebfee
s3xmaxman <badside3919@gmail.com>
2024-11-11 12:22:11 +0900

    update MemberInfoForm.tsx and page.tsx

--- src/app/profile/MemberInfoForm.tsx
+++ src/app/profile/MemberInfoForm.tsx
@@ -1,7 +1,104 @@
-import React from "react";
+"use client";
 
-const MemberInfoForm = () => {
-  return <div>MemberInfoForm</div>;
-};
+import LoadingButton from "@/components/LoadingButton";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useUpdateMember } from "@/hooks/members";
+import { requiredString } from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { members } from "@wix/members";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export default MemberInfoForm;
+const formSchema = z.object({
+  loginEmail: requiredString,
+  firstName: z.string(),
+  lastName: z.string(),
+});
+
+type FormValues = z.infer<typeof formSchema>;
+
+interface MemberInfoFormProps {
+  member: members.Member;
+}
+
+export default function MemberInfoForm({ member }: MemberInfoFormProps) {
+  const form = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      loginEmail: member.loginEmail || "",
+      firstName: member.contact?.firstName || "",
+      lastName: member.contact?.lastName || "",
+    },
+  });
+
+  const mutation = useUpdateMember();
+
+  function onSubmit(values: FormValues) {
+    mutation.mutate(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mx-auto max-w-xl space-y-5"
+      >
+        <FormField
+          control={form.control}
+          name="loginEmail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="メールアドレス"
+                  type="email"
+                  disabled
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>名前</FormLabel>
+              <FormControl>
+                <Input placeholder="名前" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>苗字</FormLabel>
+              <FormControl>
+                <Input placeholder="苗字" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <LoadingButton type="submit" loading={mutation.isPending}>
+          送信
+        </LoadingButton>
+      </form>
+    </Form>
+  );
+}
